86719a98bbb3ecfc97e424600938e0c2126e56b8
mes <mineeesel526@icloud.com>
2026-04-08 16:03:33 +0900

    Vercel 환경세팅

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 
+// 환경 변수에서 URL을 가져옵니다. 
+// Vercel(운영)에서는 설정한 값이, 로컬(.env.local)에서는 로컬 주소가 들어옵니다.
+const baseURL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: 'http://localhost/api', // next.config.ts의 rewrites를 통해 http://localhost/api 호출
+    //baseURL: 'http://localhost/api', // next.config.ts의 rewrites를 통해 http://localhost/api 호출
+    baseURL: baseURL,
     withCredentials: true/*, // 모든 요청에 쿠키 포함 설정
     headers: {
         'Content-Type': 'application/json',
---
+++ src/types/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // 여기에 추가로 사용하시는 다른 VITE_ 변수들이 있다면 똑같이 정의해주세요.
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
