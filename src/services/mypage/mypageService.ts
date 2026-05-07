b7d6f074b086a8a38d858f3d43f00f134b0ff372
mes <mineeesel526@icloud.com>
2026-05-07 15:24:10 +0900

    마이페이지 /api/auth/mypage > /auth/mypage로 원복

--- src/services/mypage/mypageService.ts
+++ src/services/mypage/mypageService.ts
@@ -5,7 +5,7 @@ export const mypageService = {
 
     getDetailProfile: async () => {
         // 토큰만 실어보냄.. reqData 아예 없는..?
-        const res = await api.post<any, ApiResData>('/api/auth/mypage');
+        const res = await api.post<any, ApiResData>('/auth/mypage');
 
         if (res.cd !== "0000") {
             throw new Error(res.msg || "상세 정보를 가져오지 못했습니다.");
