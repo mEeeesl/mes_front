f577c66d355176f046152569384d435da6cce22b
mes <mineeesel526@icloud.com>
2026-05-07 13:45:21 +0900

    Render > 토큰 > Vercel 미들웨어 (NextConfig... rewrites 설정 변경)

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
+
+    // 환경변수 값 설정 + 기본값(로컬)
+    const toUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost/api';
     return [
       {
-        source: "/api/:path*",
-        destination: "http://localhost/api/:path*", 
+        source: "/api/:path*", // 프론트엔드에서 /api로 시작하는 요청을 보내면
+        //destination: "http://localhost/api/:path*", // 실제로는 렌더 백엔드 주소로 연결
+        destination: `${toUrl}/:path*`, // 실제로는 렌더 백엔드 주소로 연결
       },
     ];
   },
